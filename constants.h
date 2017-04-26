02abd51b14fa94e02934240650a9ba94a7819076
kstarzyk <k.starzyk02@gmail.com>
2017-04-26 04:11:20 +0200

    Add constants file

---
+++ constants.h
@@ -0,0 +1,13 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_ 1
+
+namespace constants {
+  const int INF = 10000;
+  const int UNREACHABLE_TIMEOUT = 3;
+  const int DELETE_TIMEOUT = 5;
+  const std::string UNREACHABLE = "unreachable";
+}
+
+
+#endif
+
